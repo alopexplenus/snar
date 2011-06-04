89e326df238634e264418e6e4814b89c2a17fc68
niksem <niksem@niksem.ru>
2011-06-04 13:49:58 +0400

    made path to framework shorter, made snar view more detailed

--- protected/views/snar/_view.php
+++ protected/views/snar/_view.php
@@ -1,14 +1,17 @@
 <div class="view">
 
 	<?php echo CHtml::link(CHtml::encode($data->title), array('view', 'id'=>$data->id)); ?>
-	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('weight')); ?>:</b>
 	<?php echo CHtml::encode($data->weight); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('status')); ?>:</b>
-	<?php echo CHtml::encode($data->status); ?>
+	Владелец: 
+	<?php echo CHtml::encode($data->owner->username); ?> <br />
+
+	<?php 
+	$arr = Lookup::items('SnarStatus');
+	echo CHtml::encode($arr[$data->status]); ?>
 	<br />
 
 
